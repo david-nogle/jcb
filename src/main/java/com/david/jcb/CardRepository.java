0d507febde1fae295a38aeb4d0c9eaaaa9c31e60
David <david@nogle.com>
2016-05-23 10:01:21 +0800

    cards

--- src/main/java/com/david/jcb/CardRepository.java
+++ src/main/java/com/david/jcb/CardRepository.java
@@ -13,7 +13,51 @@ public class CardRepository {
 	static {
 		final Set<Form> tmp = new HashSet<>();
 		// XXX input card info here
-
+		tmp.add(Form.form()//
+				.add("txtCreditCard1", "3566")//
+				.add("txtCreditCard2", "1866")//
+				.add("txtCreditCard3", "")// XXX Optional
+				.add("txtCreditCard4", "0272")//
+				//
+				.add("txtEasyCard1", "8280")//
+				.add("txtEasyCard1", "8500")//
+				.add("txtEasyCard1", "3243")//
+				.add("txtEasyCard1", "8048")//
+				//
+				.add("captcha", "")//
+				.add("method", "loginAccept")//
+				.add("hidCaptcha", "")//
+		);
+		tmp.add(Form.form()//
+				.add("txtCreditCard1", "3566")//
+				.add("txtCreditCard2", "1821")//
+				.add("txtCreditCard3", "")// XXX Optional
+				.add("txtCreditCard4", "0276")//
+				//
+				.add("txtEasyCard1", "8280")//
+				.add("txtEasyCard1", "8500")//
+				.add("txtEasyCard1", "3243")//
+				.add("txtEasyCard1", "8055")//
+				//
+				.add("captcha", "")//
+				.add("method", "loginAccept")//
+				.add("hidCaptcha", "")//
+		);
+		tmp.add(Form.form()//
+				.add("txtCreditCard1", "3567")//
+				.add("txtCreditCard2", "3003")//
+				.add("txtCreditCard3", "")// XXX Optional
+				.add("txtCreditCard4", "4103")//
+				//
+				.add("txtEasyCard1", "8300")//
+				.add("txtEasyCard1", "9620")//
+				.add("txtEasyCard1", "0328")//
+				.add("txtEasyCard1", "3448")//
+				//
+				.add("captcha", "")//
+				.add("method", "loginAccept")//
+				.add("hidCaptcha", "")//
+		);
 		CARDS = Collections.unmodifiableSet(tmp);
 	}
 
